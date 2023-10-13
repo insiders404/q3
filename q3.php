ad410becf57063d42e93ce632033dca4f29875fc
insiders404 <147330331+insiders404@users.noreply.github.com>
2023-10-13 06:16:09 +0530

    Add files via upload

---
+++ q3.php
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="web.xsl"?>
+<employees>
+    <employee>
+        <name>John Doe</name>
+        <designation>Software Engineer</designation>
+        <age>30</age>
+        <salary>60000</salary>
+    </employee>
+    <employee>
+        <name>Jane Smith</name>
+        <designation>Product Manager</designation>
+        <age>35</age>
+        <salary>80000</salary>
+    </employee>
+    <employee>
+        <name>Alice Johnson</name>
+        <designation>Marketing Specialist</designation>
+        <age>28</age>
+        <salary>50000</salary>
+    </employee>
+</employees>
